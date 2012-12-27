14163187ceb34eea18cc81ee736eaebe0517248e
kukywin <kukywin@gmail.com>
2012-12-26 23:58:09 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   # get "static_pages/home"
   root :to => 'static_pages#home'
+  match '/signup', to: 'users#new'
   # get "static_pages/help"
   match '/help', to: 'static_pages#help'
   # get "static_pages/about"
