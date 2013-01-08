50f35c195023c527807a886af33d43488040753a
kukywin <kukywin@gmail.com>
2013-01-08 04:32:47 -0800

    eighth

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum:50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format:{ with:VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
-  validates :password, presence: true, length: { within: 6..10}
+  validates :password, length: { within: 6..10}#, presence: true 
   validates :password_confirmation, presence: true
 end
